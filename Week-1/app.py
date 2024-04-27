936f82a9e1267b7825d0fec919a810d823e38fa6
rafaelromero <rafael@rafaelromero.info>
2024-04-27 11:02:16 -0500

    Added week 1 app, a calculator

--- Week-1/app.py
+++ Week-1/app.py
@@ -2,8 +2,29 @@
 
 
 def main():
-    print("Hello, World!")
+    print("~=========Calculator App V.1.0===========~")
+    print ("Operations available:")
+    print ("1. Addition")
+    print ("2. Subtraction")
+    print ("3. Multiplication")
+    print ("4. Division")
 
+    operation = input("Enter an operation: ")
+
+    # ?? = int(input("Enter first number: "))
+    # ?? = int(input("Enter second number: "))
+
+    if operation == "1":
+        result = 0  # ??
+        print("Result: ", result)
+
+    elif operation == "2":
+        result = 0 # ??
+        print("Result: ", result)
+
+    elif operation == "3":
+        result = 0 # ??
+        print("Result: ", result)
 
 
 if __name__ == "__main__":
