e3bf1f923572e11bcab039d2656d7b1a39236e29
rafaelromero <rafael@rafaelromero.info>
2024-04-28 10:02:46 -0500

    pushing details

---
+++ Samples/template.py
@@ -0,0 +1,15 @@
+
+import sys
+
+def do_something():
+    print("Doing something")
+
+def do_something_else():
+    print("Doing something else")
+
+def main():
+    print("Hello World")
+
+
+if __name__ == "__main__":  
+    main()
---
+++ Week-1/first_program.py
@@ -0,0 +1,13 @@
+
+import pyfiglet
+
+
+
+def main():
+    message = pyfiglet.figlet_format("HELLO WORLD")
+    print(message)
+
+
+
+if __name__ == "__main__":
+    main()
