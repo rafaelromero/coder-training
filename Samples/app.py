73545d41e9de391a40ea4b3f5093669c75269a9d
rafaelromero <rafael@rafaelromero.info>
2024-12-04 20:00:29 -0600

    Added UI

--- Samples/app.py
+++ Samples/app.py
@@ -1,5 +1,13 @@
-import sys
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/fruits", methods=["GET"])
+def fruits():
+    list = ["apple", "banana", "cherry", "zebra-cakes"]
+    return jsonify(list)
 
 if __name__ == "__main__":
-    list = ["apple", "banana", "cherry", "zebra-cakes"]
-    print(list[0],"\n",list[1], "\n",list[2], "\n",list[3])
+    app.run()
